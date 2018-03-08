00fa127b4d2a20bb6c0eb885c43da39ed61582ed
Zhang Matt <mzhang@lcs.on.ca>
2018-03-08 09:13:29 -0500

    completed question 1!

--- ProjectEuler.playground/Contents.swift
+++ ProjectEuler.playground/Contents.swift
@@ -2,4 +2,27 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+var i : Int = 0
+var total : Int = 0
+//add 3, add 5, remove 15
+var numbers : [Int] = []
+
+for _ in 1...999
+{
+    i += 1
+    if i%3 == 0
+    {
+        numbers.append(i)
+    }
+    else if i%5 == 0
+    {
+        numbers.append(i)
+    }
+}
+
+for x in 0...numbers.count-1
+{
+    total += numbers[x]
+}
+numbers
+print(total)
